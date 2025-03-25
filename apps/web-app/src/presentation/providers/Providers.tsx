dd02e41d891f323009292ae6b075f4b0932577ba
Adrien <ala@ucaya.com>
2025-03-25 20:52:00 +0100

    Update libs & clean code

--- apps/web-app/src/presentation/providers/Providers.tsx
+++ apps/web-app/src/presentation/providers/Providers.tsx
@@ -1,7 +1,8 @@
-import { I18nProvider } from '@/presentation/providers/I18nProvider'
 import type { FC, PropsWithChildren } from 'react'
 import { RouterProvider } from 'react-aria-components'
 import { useNavigate } from 'react-router'
+
+import { I18nProvider } from '@/presentation/providers'
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
   const navigate = useNavigate()
